bbe94d556ca96400f9d21b848d7c3ce6aa9f5ab7
Joesac <sackeyj10@gmail.com>
2023-04-03 13:50:34 +0000

    image paths are now working but not all

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -15,35 +15,35 @@ export class HomeComponent {
   places: {id: string, img: string, desc: string, price: number, perPerson: number}[] = [
     {
       id: "1",
-      img: '/assets/place1.jpg',
+      img: './assets/place1.jpg',
       desc: 'Shimia',
       price: 12354,
       perPerson: 2342
     },
     {
       id: '2',
-      img: '/assets/place2.jpg',
+      img: './assets/place2.jpg',
       desc: 'Goa',
       price: 4355,
       perPerson: 198
     },
     {
       id: '3',
-      img: '/assets/place3.jpg',
+      img: './assets/place3.jpg',
       desc: 'Brazil',
       price: 57653,
       perPerson: 767
     },
     {
       id: '4',
-      img: '/assets/place4.jpg',
+      img: './assets/place4.jpg',
       desc: 'Hamburg',
       price: 87673,
       perPerson: 876
     },
     {
       id: '5',
-      img: '/assets/place5.jpg',
+      img: './assets/place5.jpg',
       desc: 'Standford',
       price: 98786,
       perPerson: 234
